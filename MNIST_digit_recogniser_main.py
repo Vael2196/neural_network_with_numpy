b3cd75b90b68529a78f110b7732102bac3cc9889
Vadim Filyakin <vadimfilyakin@gmail.com>
2024-08-08 21:28:19 +1000

    Placing data into a matrix and transpositioning it for a sake of simplicity

--- MNIST_digit_recogniser_main.py
+++ MNIST_digit_recogniser_main.py
@@ -8,3 +8,18 @@ from matplotlib import pyplot as ppl
 
 data = pnd.read_csv("Dataset/train.csv")
 data.head()
+
+data = np.array(data)
+m, n = data.shape #dimensions of the data, where m is the number of rows and n is the amount of features +1
+np.random.shuffle(data)
+
+data_dev = data[0:1000].T #data transposition
+
+y_dev = data_dev[0]
+x_dev = data_dev[1:n]
+
+data_train = data[1000:m].T
+y_train = data_train[0]
+x_train = data_train[1:n]
+
+print(x_train[:, 0].shape) #test the first column
